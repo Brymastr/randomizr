c07de523eaa293ebb75aaae934a5eb7984036b6a
Brycen Dorsay <dorsay@live.ca>
2015-01-15 22:18:06 -0800

    table creation migrations

---
+++ app/views/index.blade.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Brycen
+ * Date: 2015-01-14
+ * Time: 5:39 PM
+ */
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Randomizr</title>
+</head>
+<body>
+    <h1>Randomizr</h1>
+    <h2>Randomizer of sites</h2>
+    {{ Form::url('sites') }}
+    {{ Form::url('sites') }}
+    {{ Form::url('sites') }}
+    {{ Form::url('sites') }}
+    {{ Form::url('sites') }}
+</body>
+</html>
