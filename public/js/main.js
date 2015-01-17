d695373ce40df155e8bf921a7b608992f393e67d
Brycen Dorsay <dorsay@live.ca>
2015-01-17 04:49:38 -0800

    Random background color on page load
    Colors are all the number 4 color of each pallet from the android material design colour swatches

--- public/js/main.js
+++ public/js/main.js
@@ -20,3 +20,34 @@ $('#more-links').click(function() {
     }
 });
 
+$(document).ready(function() {
+
+    var color = '';
+
+    var colors = [
+        '#EF5350',
+        '#EC407A',
+        '#AB47BC',
+        '#7E57C2',
+        '#5C6BC0',
+        '#42A5F5',
+        '#29B6F6',
+        '#26C6DA',
+        '#26A69A',
+        '#66BB6A',
+        '#9CCC65',
+        '#D4E157',
+        '#FFEE58',
+        '#FFCA28',
+        '#FFA726',
+        '#FF7043',
+        '#8D6E63',
+        '#BDBDBD',
+        '#78909C',
+    ];
+
+    color += colors[Math.floor(Math.random() * colors.length)];
+
+    $('body').css({'background': color});
+});
+
